45efaa2f6b4ec41d4ba9b1cc54bc3207cb4f1be5
GojurJeevan <jeevangojur2605@gmail.com>
2025-08-13 14:06:23 +0530

    operators

---
+++ Operators/operators.js
@@ -0,0 +1,22 @@
+/* 
+Operators:
+   Special sign used to perform operation on operands
+Why:
+    mathematical calculations,decision making,manipulate data
+
+Types:
+
+1.Arthimetic operator:
+    +,-,*,/,**,%
+2.Assignment operator:
+    =,+=,-=,*=,/=,%=,**=
+3.Comparision Operator:
+    ==,!=,>,<,>=,<=,===,!==
+
+
+
+
+
+
+
+*/
